f5abb2f585a3ef27e64819dcac98dc26ceb57192
ankushKun <ankush4singh@gmail.com>
2025-07-27 11:11:51 +0530

    Add SDK and wauth as submodules, update dependencies to use local paths
    - Add @subspace-protocol/sdk as submodule from git@github.com:subspace-dev/sdk.git
    - Add wauth repository as submodule from git@github.com:subspace-dev/wauth.git
    - Update package.json to use file:./sdk instead of @subspace-protocol/sdk npm package
    - Update package.json to use file:./wauth/sdk instead of @wauth/sdk npm package
    - Update package.json to use file:./wauth/strategy instead of @wauth/strategy npm package
    - Update dev:sdk script to use ./sdk path instead of ../sdk

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.evfjf9d44bg"
+    "revision": "0.n3nft5k05g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
